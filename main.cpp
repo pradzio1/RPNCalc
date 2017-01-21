bac3fe87600d1b255aaa686bbb357579947dee81
Radix <pieja.radoslaw@gmail.com>
2017-01-21 18:14:40 +0100

    Declarations separated from definitions, added a greeter message.

--- main.cpp
+++ main.cpp
@@ -8,6 +8,45 @@
 #include "MultiplyOperatorCommand.h"
 #include "PushNumberCommand.h"
 
+void showStack(std::stack<double>);
+
+bool isValid(std::string &);
+
+Command *createCommand(std::string);
+
+void Greeter();
+
+
+int main() {
+    Greeter();
+    std::stack<double> operationStack;
+    std::string input;
+    Command *command;
+    while (true) {
+        std::cin >> input;
+        if (input == "c")
+            break;
+        else {
+            try {
+                command = createCommand(input);
+            }
+            catch (OperationException exc) {
+                exc.what();
+                command = NULL;
+            }
+            if (command)
+                try {
+                    command->execute(operationStack);
+                }
+                catch (OperationException exc) {
+                    exc.what();
+                }
+        }
+        showStack(operationStack);
+    }
+    return 0;
+}
+
 void showStack(std::stack<double> s) {
     while (!s.empty()) {
         std::cout << "> " << s.top() << std::endl;
@@ -55,31 +94,7 @@ Command *createCommand(std::string c) {
         throw OperationException(OperationException::INVALID_ARGUMENT);
 }
 
-int main() {
-    std::stack<double> operationStack;
-    std::string input;
-    Command *command;
-    while (true) {
-        std::cin >> input;
-        if (input == "c")
-            break;
-        else {
-            try {
-                command = createCommand(input);
-            }
-            catch (OperationException exc) {
-                exc.what();
-                command = NULL;
-            }
-            if (command)
-                try {
-                    command->execute(operationStack);
-                }
-                catch (OperationException exc) {
-                    exc.what();
-                }
-        }
-        showStack(operationStack);
-    }
-    return 0;
+void Greeter() {
+    std::cout
+            << "Welcome to RPNCalc\nRPNCalc is basic calculator\nusing Reversed Polish Notation\n\nTo start just type number or operator\nand press Enter\n\nEnter \"c\" to quit\n";
 }
